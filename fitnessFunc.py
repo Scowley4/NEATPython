9f3c6023db128a1f34400701abe135ac9d805977
djpassey <djpasseyjr@gmail.com>
2019-03-06 13:22:24 -0700

    Updated Xor Dparity

--- fitnessFunc.py
+++ fitnessFunc.py
@@ -5,8 +5,9 @@ del sys.modules['flappyNeat']
 from flappyNeat import main
 import sys
 from scipy.integrate import odeint
+from networkx import DiGraph,has_path
 
-def fit_xor(network):
+def fit_xor(network,complexity=False):
     """
     Function for determining how well the network solves the
     exclusive or problem.
@@ -20,19 +21,29 @@ def fit_xor(network):
     fitness (a fitness score)
     """
     samples = 4
-    X = np.array([[0,0],[1,0],[0,1],[1,1]])
-    Y = np.array([0,1,1,0])
-
-    outputs = np.zeros(samples)
-    for i in range(len(X)):
-        try:
-            outputs[i] = network.activate(X[i])
-        except:
-            print(network.activate(X[i]))
-            sys.exit()
+    xor_inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    xor_outputs = [0, 1, 1, 0]
 
 
-    fitness = np.sum(Y*(outputs>.5))/float(samples)
+    sum_square_error = 0.0
+    for inputs, expected in zip(xor_inputs, xor_outputs):
+        # Serial activation propagates the inputs through the entire network.
+        output = network.activate(inputs)
+        sum_square_error += (output[0] - expected) ** 2
+
+    # When the output matches expected for all inputs, fitness will reach
+    # its maximum value of 1.0.
+    fitness = 4 - sum_square_error
+
+    if complexity:
+        # Add to the fitness if the inputs are connected to the outputs
+        G = DiGraph(network.A.T)
+        if has_path(G,0,3) and has_path(G,1,3):
+            fitness += con_weight
+
+        # Add to fitness if there are more than 3 edges
+        if (network.A != 0).sum() > 3:
+            fitness += .10
 
     return fitness
 
@@ -50,7 +61,7 @@ def get_determined_fit_dparity(num_inputs):
         for i in range(samples):
             outputs[i] = network.activate(X[i])
 
-        fitness = np.sum(Y*outputs)/float(samples)
+        fitness = samples - np.sum((Y - outputs)**2)
         return fitness
 
     return determined_fit_dparity
@@ -84,7 +95,7 @@ def get_fit_dparity(num_inputs, samples=100):
         for i in range(samples):
             outputs[i] = network.activate(X[i])
 
-        fitness = np.sum(Y*outputs)/float(samples)
+        fitness = samples - np.sum((Y - outputs)**2)
         return fitness
 
     return fit_dparity
@@ -143,7 +154,7 @@ def fit_pole_balance(network,
 
     Parameters
     ----------
-    network (neat network object) that accepts four inputs and has one output
+    network (neat network object) that accepts two inputs and has one output
     other parameters are explained in the paper
     max_time (int) the longest a network is permitted to keep the pole upright in seconds
 
@@ -198,7 +209,15 @@ def fit_pole_balance(network,
 
     while (abs(x) < track_limit) and (abs(theta) < pole_angle_failure) and (t[0] < max_time):
         y0 = np.array([x,x_vel,theta,theta_vel])
-        force = network.activate([x,x_vel,theta,theta_vel])
+        
+        # Network chooses the direction to push
+        push = network.activate([x,theta])
+        
+        if push > .5:
+            force = 1.
+        else:
+            force = -1.
+            
         F = makeSystem(force)
         # Simulate system
         x,x_vel,theta,theta_vel = odeint(F,y0,t)[1]
@@ -206,3 +225,74 @@ def fit_pole_balance(network,
 
     return t[0]
 
+def balance_pole(network,
+                 max_time=120,
+                 grav=-9.81,
+                 pole_len=.5,
+                 track_limit=2.4,
+                 pole_angle_failure=.209,
+                 cart_mass=1.,
+                 pole_mass=.1,
+                 time_step=.02):
+    
+    def makeSystem(force):
+
+        #Differential equations (I've checked these by hand):
+
+        def angular_accel(theta,theta_vel):
+
+            # This equation comes from the paper cited above
+
+            a = grav*np.sin(theta)+np.cos(theta)
+            b = (-1*force - pole_mass*pole_len*(theta_vel**2)*np.sin(theta))/(cart_mass+pole_mass)
+            c = pole_len*(4./3 - ((pole_mass*(np.cos(theta))**2)/cart_mass+pole_mass))
+            theta_acc = a*b/float(c)
+
+            return theta_acc
+
+        def cart_accel(theta,theta_vel,theta_acc):
+
+            # This equation comes from the paper above
+
+            a = (theta_vel**2)*np.sin(theta) - theta_acc*np.cos(theta)
+            x_acc = (force + pole_mass*pole_len*a)/(cart_mass+pole_mass)
+
+            return x_acc
+
+        def F(x,t):
+            theta_acc = angular_accel(x[2],x[3])
+            x_acc = cart_accel(x[2],x[3],theta_acc)
+            return np.array([x[1], x_acc, x[3], theta_acc])
+
+        return F
+
+    # Initial conditions:
+    theta = 0
+    theta_vel = 0
+    theta_acc = 0
+    x = 0
+    x_vel = 0
+    x_acc = 0
+    t = np.linspace(0,time_step,2)
+    force = 0
+
+
+    while (abs(x) < track_limit) and (abs(theta) < pole_angle_failure) and (t[0] < max_time):
+        y0 = np.array([x,x_vel,theta,theta_vel])
+        print(theta,x,force)
+        # Network chooses the direction to push
+        push = network.activate([x,x_vel,theta,theta_vel])
+        
+        if push > .5:
+            force = 10
+        else:
+            force = -10
+            
+        F = makeSystem(force)
+        # Simulate system
+        x,x_vel,theta,theta_vel = odeint(F,y0,t)[1]
+        t = np.linspace(t[1],t[1]+time_step,2)
+
+    return t[0]
+    
+    
